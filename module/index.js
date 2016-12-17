de67a2d2856ec666f553ea1af3828d6ca726e966
Patrick Balsiger <frupi@gmail.com>
2016-12-17 15:01:05 +0100

    formatting

--- module/index.js
+++ module/index.js
@@ -2,12 +2,12 @@ module.exports = {
     context: require('./core/context'),
     server: require('./core/server'),
     logAppenders: [
-        require('./log/consoleAppender.js')
-        //require('./log/fileAppender.js')
+      require('./log/consoleAppender.js')
+      //require('./log/fileAppender.js')
     ],
     run: function(config, ctx){
-		var appContext = this.context.build(config, ctx);
-		var httpServer = this.server.httpd.start(appContext);
-		return appContext;
+  		var appContext = this.context.build(config, ctx);
+  		var httpServer = this.server.httpd.start(appContext);
+  		return appContext;
     }
 };
