14e86df89811b57d0e3ae9f4dada1ef806ca5960
Patrick Balsiger <frupii@gmail.com>
2017-01-08 22:47:36 +0100

    change default log level, version bump

--- module/log/logger.js
+++ module/log/logger.js
@@ -7,10 +7,10 @@ var Enum = require("../util/enum");
  * Use this logger to log "log4J"-style.
  * It emitts events so the appenders function.
  * Supported levels at the moment: TRACE, ERROR, INFO, DEBUG, FATAL
- * 
+ *
  * NOTE: This class is still under development at the moment.
  */
- 
+
 var LEVEL = Enum.define({
     TRACE   : { value : 0, string : 'TRACE' },
     DEBUG   : { value : 1, string : 'DEBUG' },
@@ -20,16 +20,16 @@ var LEVEL = Enum.define({
 });
 
 module.exports = {
-    
+
     config : {
-        logLevel : "TRACE"
+        logLevel : "INFO"
     },
     emitter : new EventEmitter(),
-    
+
     trace : function(message) {
 		this.printMessage(LEVEL.TRACE.string, message);
 	},
-    
+
     debug : function(message) {
 		this.printMessage(LEVEL.DEBUG.string, message);
 	},
@@ -40,14 +40,14 @@ module.exports = {
 
     error : function(message) {
 		this.printMessage(LEVEL.ERROR.string, message);
-	},    
-    
+	},
+
     fatal : function(message) {
 		this.printMessage(LEVEL.FATAL.string, message);
 	},
-    
+
     printMessage : function(level, message) {
-        if(LEVEL.getByName(this.config.logLevel).value <= LEVEL.getByName(level).value){            
+        if(LEVEL.getByName(this.config.logLevel).value <= LEVEL.getByName(level).value){
             var now = new Date();
             message = now.toISOString()+" [" + level + "] " + message;
             this.emitter.emit('append',message);
